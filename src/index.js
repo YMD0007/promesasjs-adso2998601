1f5ca5739929be244433903b5a49f9a9efdcf35b
YMD0007 <yeyemejia18+1@gmail.com>
2025-05-21 15:17:39 -0500

    ejemplos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 
 
 
+// function promise () {
+//     return new Promise((resolve, reject) => {
+//         let error = false;
+//         if (error) {
+//             resolve('todo en orden');
+//         } else {
+//             reject('algo paso con la promesa');
+//         }
+//     });
+// };
+
+// promise()
+//     .then(mensaje => console.log(mensaje))
+//     .catch(error => console.log(error));
 
 
 
+//crear una promesa que se rechace depues de 3 segundos si un numero aleatorio es menor a 0.5, si no que se resuelva
+let numero = Math.random();
+const promesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (numero < 0.5) {
+            resolve('error numero menor');
+        } else {
+            reject('el numero es mayor');
+        }
+    }, 1000);
+});
+
+promesa
+    .then(mensaje => {console.log(mensaje, numero)})
+    .catch(error => {console.log(error, numero);});
