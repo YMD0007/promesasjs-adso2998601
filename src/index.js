ee93bfad044f26a11cde5df855108b392b0b2293
YMD0007 <yeyemejia18+1@gmail.com>
2025-05-21 14:29:39 -0500

    creamos la rama ejercicios

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-// // funcion con async 
-
-function descargarArchivo() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("promesa ejecutada con exito despues de 8 segundos 👻😊");
-        }, 8000);
-    });
-}
-descargarArchivo()
-.then((response) => {
-    console.log(response);
-})
-
-
-// async function ejecutar() {
-
-// try {
-//     console.log("⌛⌛descarga en progreso")
-//     const response = await decargarArchivo();
-//     console.log(response);
-// }catch (error) {
-//     console.error("error", error);
-    
-// }
-// }
 
 
 
